08797aa459f9c28f7c695c403a51944a7857b67b
Balint Rozsa <76824890+Balint-R@users.noreply.github.com>
2023-02-05 23:52:47 -0500

    Simplify input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <numeric>
 #include <algorithm>
 using namespace std;
 
-typedef unsigned int uint;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
@@ -132,13 +130,8 @@ struct Input {
     template <typename T, typename U> requires (!is_same_v<T, U> || is_floating_point_v<T>)
     ll readInt(T minValid, U maxValid){return readInt<ll>((ll) minValid, (ll) maxValid);}
 
-    static constexpr int MDGT = 20;
-    static constexpr ull PWS[MDGT] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, (ull) 1e8, (ull) 1e9, (ull) 1e10,
-            (ull) 1e11, (ull) 1e12, (ull) 1e13, (ull) 1e14, (ull) 1e15, (ull) 1e16, (ull) 1e17, (ull) 1e18, (ull) 1e19};
-    template <typename T = double, typename U = ull>
-    requires (is_floating_point_v<T> && is_integral_v<U> && is_unsigned_v<U> && numeric_limits<U>::digits10 < MDGT)
+    template <typename T = double> requires (is_floating_point_v<T>)
     T readFloat(int reqPrec = -1){
-        assertIe(reqPrec <= numeric_limits<U>::digits10, "Required precision is more than can be parsed");
         if constexpr(!isIdentical) eatSpace();
         bool sign = peekChar() == '-'; incPtr(sign);
 
@@ -146,34 +139,17 @@ struct Input {
         if(peekChar() == '0') res = 0;
         else {
             assertPe(peekChar() >= '0' && peekChar() <= '9', "Invalid float");
-            U x = getChar()-'0';
-
-            while(peekChar() >= '0' && peekChar() <= '9'){
-                if(x >= numeric_limits<U>::max()/10){
-                    res = x;
-                    while(peekChar() >= '0' && peekChar() <= '9') res = res*10 + (getChar()-'0');
-                    goto finishedParsingWhole; // skips res = x
-                }
-                x = x*10 + getChar()-'0';
-            }
-
-            res = x;
-            finishedParsingWhole:;
+            res = getchar()-'0';
+            while(peekChar() >= '0' && peekChar() <= '9') res = res*10 + (getChar()-'0');
         }
 
+        int prec = 0;
         if(peekChar() == '.'){
             incPtr();
-            assertPe(peekChar() >= '0' && peekChar() <= '9', "Invalid float");
-            U y = getChar()-'0';
-            int prec = 1;
-            while(peekChar() >= '0' && peekChar() <= '9'){
-                if(prec < numeric_limits<U>::digits10) y = y*10 + peekChar()-'0', prec++;
-                incPtr();
-            }
-            assertPe(reqPrec < 0 || reqPrec == prec, "Float must have exactly %d digits after the decimal", reqPrec);
-            res += (T) y/PWS[prec];
+            for(T pw = (T) 1/10; peekChar() >= '0' && peekChar() <= '9'; pw /= 10, prec++) res += (getchar()-'0')*pw;
+            assertPe(prec, "Invalid float");
         }
-        else assertPe(reqPrec <= 0, "Float must have exactly %d digits after the decimal", reqPrec);
+        assertPe(reqPrec < 0 || reqPrec == prec, "Float must have exactly %d digits after the decimal", reqPrec);
 
         assertPe(isDelim(peekChar()), "Invalid float");
         return sign ? -res : res;
@@ -193,57 +169,19 @@ struct Input {
         readNl();
         return {buf + beg, ii - beg};
     }
-
-    template <typename T = ll> requires is_integral_v<T>
-    T readSingleInt(T minValid = numeric_limits<T>::min(), T maxValid = numeric_limits<T>::max()){
-        T x = readInt(minValid, maxValid);
-        readNl();
-        return x;
-    }
-    template <typename T, typename U> requires (!is_same_v<T, U> || is_floating_point_v<T>)
-    ll readSingleInt(T minValid, U maxValid){return readSingleInt<ll>(minValid, maxValid);}
-
-    template <typename T = ll> requires is_integral_v<T>
-    pair<T, T> readIntPair(T minValid = numeric_limits<T>::min(), T maxValid = numeric_limits<T>::max()){
-        T x = readInt(minValid, maxValid); readSp();
-        T y = readInt(minValid, maxValid); readNl();
-        return {x, y};
-    }
-    template <typename T, typename U> requires (!is_same_v<T, U> || is_floating_point_v<T>)
-    pair<ll, ll> readIntPair(T minValid, U maxValid){return readIntPair<ll>(minValid, maxValid);}
-
-    template <int sz, typename T = ll> requires (sz > 0 && is_integral_v<T>)
-    array<T, sz> readIntTuple(T minValid = numeric_limits<T>::min(), T maxValid = numeric_limits<T>::max()){
-        array<T, sz> res{readInt<T>(minValid, maxValid)};
-        for(int i = 1; i < sz; i++) readSp(), res[i] = readInt<T>(minValid, maxValid);
-        readNl();
-        return res;
-    }
-    template <int sz, typename T, typename U> requires (sz > 0 && (!is_same_v<T, U> || is_floating_point_v<T>))
-    array<ll, sz> readIntTuple(T minValid, U maxValid){return readIntTuple<sz, ll>(minValid, maxValid);}
-
-    template <typename T, typename U, typename V> requires is_integral_v<T>
-    void readIntArray(T *ptr, int sz, U minValid = numeric_limits<T>::min(), V maxValid = numeric_limits<T>::max()){
-        assertIe(sz > 0, "Non-positive array size");
-        ptr[0] = readInt<T>(minValid, maxValid);
-        for(int i = 1; i < sz; i++) readSp(), ptr[i] = readInt<T>(minValid, maxValid);
-        readNl();
-    }
 };
 
-int arr[100];
+using Validator = Input<true, true, false>;
+using IdenticalReader = Input<false, true, false>;
+using StandardReader = Input<false, false, false>;
+using IdenticalInteractor = Input<false, true, true>;
+using StandardInteractor = Input<false, false, true>;
 
-int main(){
-    //return !printf("%hhd\n%hhd\n%hhd\n%hhd\n%hhd\n%hhd\n", '\t', '\n', '\v', '\f', '\r', ' ');
-
-    Input<false, false, false> inp(stdin);
-    int n = inp.readInt(); inp.readNl();
-    int x = inp.readInt<__int128, unsigned __int128>(1, 30); inp.readNl();
-
-    inp.readSingleInt(1, 1000LL);
-    inp.readIntArray(arr, 3, -1e2, 1e2);
-    for(int i = 0; i < 3; i++) printf("%d%c", arr[i], " \n"[i == n-1]);
-    inp.readEOF();
+int main(int argc, char **argv){
+    assertIe(argc > 3, "Not enough arguments");
+    Validator judgeIn(fopen(argv[1], "r"));
+    IdenticalReader userOut(fopen(argv[2], "r"));
+    Validator judgeOut(fopen(argv[3], "r"));
+    
+    
 }
-
-// reviewed whole thing
